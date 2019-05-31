066d785bf967687ea0f349d36253cc437da7b69e
cyt <1064786402@qq.com>
2019-05-31 21:22:25 +0800

    change main.js

--- main/main.js
+++ main/main.js
@@ -1,4 +1,18 @@
-module.exports = function main() {
+module.exports = function main(str) {
     console.log("Debug Info");
-    return 'Hello World!';
+    // return 'Hello World!';
+    var lcd = [ ['._.','...','._.','._.','...','._.','._.','._.','._.','._.'],
+				['|.|','..|','._|','._|','|_|','|_.','|_.','..|','|_|','|_|'],
+				['|_|','..|','|_.','._|','..|','._|','|_|','..|','|_|','..|'] ];
+	var result = '';
+	for (var i = 0; i < 3; i++) {
+		for (var j = 0; j < str.length; j++) {
+			result += lcd[i][parseInt(str.charAt(j))];
+			if (j!=str.length-1) {
+				result += ' ';
+			}
+		}
+		result += '\n';
+	}
+	return result;
 };
